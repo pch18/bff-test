d60c402d001fa4cc81283c1b7cba31ffcdecb79e
潘晨昊 <panchenhao@bytedance.com>
2023-03-12 23:09:21 +0800

    表格demo

--- projects/web/src/pages/SiteList/index.tsx
+++ projects/web/src/pages/SiteList/index.tsx
@@ -1,16 +1,58 @@
-import { Button, Drawer } from "@arco-design/web-react";
+import {
+  Button,
+  Drawer,
+  Table,
+  TableColumnProps,
+} from "@arco-design/web-react";
 import { openSiteDrawer } from "../../common/SiteDetailModel";
+import { useAntdTable, useRequest } from "ahooks";
 
+const columns: TableColumnProps[] = [
+  {
+    title: "#",
+    dataIndex: "id",
+  },
+  {
+    title: "域名",
+    dataIndex: "domain",
+  },
+  {
+    title: "状态",
+    dataIndex: "status",
+  },
+  {
+    title: "主目录",
+    dataIndex: "root",
+  },
+  {
+    title: "证书日期",
+    dataIndex: "sslExp",
+  },
+  {
+    title: "备注",
+    dataIndex: "extraInfo",
+  },
+];
 export default function () {
+  const { tableProps } = useAntdTable(async () => {
+    return {
+      total: 11,
+      list: [{ name: 1 }],
+    };
+  });
   return (
     <div className="p-4">
-      <Button
-        onClick={async () => {
-          console.log(await openSiteDrawer({ siteKey: "123" }));
-        }}
-      >
-        新建站点
-      </Button>
+      <div className="mb-3">
+        <Button
+          onClick={async () => {
+            console.log(await openSiteDrawer({ siteKey: "123" }));
+          }}
+        >
+          新建站点
+        </Button>
+      </div>
+
+      <Table columns={columns} {...tableProps} />
     </div>
   );
 }
